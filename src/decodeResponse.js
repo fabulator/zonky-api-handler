a9068ec00ea86ac51faa6023835ef56237022650
Michal Ozogán <michal@ozogan.eu>
2018-10-17 14:24:22 +0200

    Update flow

--- src/decodeResponse.js
+++ src/decodeResponse.js
@@ -1,6 +1,11 @@
 // @flow
 export default function decodeResponse(response: Response): Promise<string | Buffer> {
-    const contentType: string = response.headers.get('content-type');
+    const contentType: ?string = response.headers.get('content-type');
+
+    if (!contentType) {
+        // $FlowFixMe
+        return response.buffer();
+    }
 
     if (contentType.indexOf('json') >= 0) {
         return response.json();
